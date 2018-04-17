fe1ff22c6e2ae44c7c8da34d64358184f612b114
littleIQ <niyuxi@imudges.com>
2018-04-18 06:17:53 +0800

    init

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ var app = express();
 var path = require('path');
 var update = require('./update');
 
-update('魔宙');
 
 app.use(express.static(path.join(__dirname)));
 
 app.get('/', function (req, res) {
+  update('魔宙');
   res.send('Hello World!');
 });
 
 app.listen(process.env.PORT || 3000, function () {
-  console.log('Example app listening on port '+(process.env.PORT || 5000)+'!');
+  console.log('Example app listening on port '+(process.env.PORT || 3000)+'!');
 });
